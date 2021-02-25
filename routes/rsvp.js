082386d2952757e096a2e72d9d496d1470218e1a
UUUZ <chuyuzi428@gmail.com>
2021-02-25 10:51:11 -0800

    updated map, camera, and rsvp form

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -8,3 +8,14 @@ var data = {
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
+
+exports.addRSVP = function(request, response){
+    var rsvpEmail = request.body.rsvpEmail;
+    console.log(rsvpEmail);
+
+    //add to current data
+    data.rsvp.push(rsvpEmail);
+
+    //send back data to the browser
+    response.send(rsvpEmail);
+}
